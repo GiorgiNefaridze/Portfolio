ba09cdc66c1637db14fdd27279e7797933bfbe66
Giorgi Nefaridze <nefogio1@gmail.com>
2022-06-29 21:27:50 +0400

    giving meaningful name

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -16,13 +16,13 @@ export default function Projects() {
             <div className='projects-container-inner'>
                 <p>Projects</p>
                 <div className='projects-container-inner-content'>
-                    {projects.map(item => (
-                        <div key={item.id} className='project'>
-                            <img src={item.poster} alt="poster"/>
+                    {projects.map(language => (
+                        <div key={language.id} className='project'>
+                            <img src={language.poster} alt="poster"/>
                             <div className='project-inner'>
-                                <span>{item.name}</span>
+                                <span>{language.name}</span>
                                 <div>
-                                    <button onClick={()=> seeProject(item.liveVersion)}>
+                                    <button onClick={()=> seeProject(language.liveVersion)}>
                                         See
                                         <svg
                                             fill="white"
@@ -33,7 +33,7 @@ export default function Projects() {
                                             clipRule="evenodd"><path
                                             d="M12.01 20c-5.065 0-9.586-4.211-12.01-8.424 2.418-4.103 6.943-7.576 12.01-7.576 5.135 0 9.635 3.453 11.999 7.564-2.241 4.43-6.726 8.436-11.999 8.436zm-10.842-8.416c.843 1.331 5.018 7.416 10.842 7.416 6.305 0 10.112-6.103 10.851-7.405-.772-1.198-4.606-6.595-10.851-6.595-6.116 0-10.025 5.355-10.842 6.584zm10.832-4.584c2.76 0 5 2.24 5 5s-2.24 5-5 5-5-2.24-5-5 2.24-5 5-5zm0 1c2.208 0 4 1.792 4 4s-1.792 4-4 4-4-1.792-4-4 1.792-4 4-4z"/></svg>
                                     </button>
-                                    <button className={item.codeURL === "" ? "withoutCode" : ""} onClick={()=> codeOfProject(item.codeURL)}>
+                                    <button className={language.codeURL === "" ? "withoutCode" : ""} onClick={()=> codeOfProject(language.codeURL)}>
                                         Code
                                         <svg
                                             fill="white"
